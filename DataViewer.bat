c7de7d46be22ee2680c29b79fa20683c823a1fc4
PandaSense <d3nick@gmail.com>
2015-07-06 21:51:14 +0800

    initialize project

---
+++ DataViewer.bat
@@ -0,0 +1,2 @@
+@echo on
+start javaw -Xmx512m -cp ".\lib\autocomplete.jar;.\lib\db2jcc.jar;.\lib\db2jcc_license_cisuz.jar;.\lib\fife.common.jar;.\lib\htmllexer.jar;.\lib\htmlparser.jar;.\lib\iText-2.1.7.jar;.\lib\iText-rtf-2.1.7.jar;.\lib\log4j-1.2.15.jar;.\lib\poi-3.2-FINAL-20081019.jar;.\lib\rsyntaxtextarea.jar;.\lib\swingx-1.6.1.jar;.\lib\DataViewerLauncher.jar" com.dv.ui.DataViewer
